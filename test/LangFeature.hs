9038a8b4542bd2b4d5be62be9904c2a0a7c59cf6
dainslef <dainslef@outlook.com>
2020-05-20 20:16:56 +0800

    Update the language feature test.

--- test/LangFeature.hs
+++ test/LangFeature.hs
@@ -18,33 +18,37 @@ import           Lang.TypeClass
 import           Lang.TypeFamily
 
 main :: IO ()
-main = hspec $ describe "Language Feature Test" $ do
-  it "Async"                 testAsync1
-  it "CSP"                   testCSP
-  it "DuplicateRecordFields" testDuplicateRecordFields
-  it "Exception" $ do
-    testException1
-    testException2
-    testException3
-  it "FixityDeclaration" testFixityDeclaration
-  it "GADT"              testGADT
-  it "Http"              testHttp
-  it "Lazy" $ do
-    testLazy1
-    testLazy2
-    testLazy3
-    testLazy4
-    testLazy5
-  it "Map"          testCustomMap
-  it "Maybe"        testMaybe
-  it "Monad"        testMonad
-  it "PatternMatch" testPatternMatch
-  it "RankNType"    testRankNTypes
-  it "STM" $ testSTM1 >> testSTM2
-  it "TChan" $ testTChan1 >> testTChan2
-  it "TypeClass" $ do
-    testTypeClass1
-    testTypeClass2
-  it "FunctionalDependencies" $ do
-    testFunctionalDependencies1
-    testFunctionalDependencies2
+main = hspec $ do
+
+  describe "Language Feature Test" $ do
+    it "Async"                 testAsync1
+    it "CSP"                   testCSP
+    it "DuplicateRecordFields" testDuplicateRecordFields
+    it "FixityDeclaration"     testFixityDeclaration
+    it "GADT"                  testGADT
+    it "Http"                  testHttp
+    it "Map"                   testCustomMap
+    it "Maybe"                 testMaybe
+    it "Monad"                 testMonad
+    it "PatternMatch"          testPatternMatch
+    it "RankNType"             testRankNTypes
+    it "STM" $ testSTM1 >> testSTM2
+    it "TypeClass" $ do
+      testTypeClass1
+      testTypeClass2
+    it "FunctionalDependencies" $ do
+      testFunctionalDependencies1
+      testFunctionalDependencies2
+
+  describe "Pending Test" $ do
+    xit "Exception" $ do
+      testException1
+      testException2
+      testException3
+    xit "TChan" $ testTChan1 >> testTChan2
+    xit "Lazy" $ do
+      testLazy1
+      testLazy2
+      testLazy3
+      testLazy4
+      testLazy5
