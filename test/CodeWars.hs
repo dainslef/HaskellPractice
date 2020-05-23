9216cdf1549188025dfc41601a23918048edb1cc
dainslef <dainslef@outlook.com>
2020-05-23 20:21:11 +0800

    Update some test.

--- test/CodeWars.hs
+++ test/CodeWars.hs
@@ -1,10 +1,14 @@
 import           Test.Hspec
 import           CodeWars.Kata.SplitStrings
-import           CodeWars.Kata.UniqueInOrder
-import           CodeWars.Kata.HumanReadableDurationFormat
+
+-- import           CodeWars.Kata.RailFenceCipherEncodingAndDecoding
+-- import           CodeWars.Kata.UniqueInOrder
+-- import           CodeWars.Kata.HumanReadableDurationFormat
 
 main :: IO ()
 main = hspec $ describe "Code Wars" $ do
   testSplitStrings
+
+  -- testRailFenceCipherEncodingAndDecoding
   -- testUniqueInOrder
   -- testFormatDuration
